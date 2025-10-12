9f15186ac1f8e91d01e0be669143206a867a5f15
Aaron Wijesinghe <aaronw1975@icloud.com>
2025-10-12 16:04:09 -0400

    Add functional downloading code (for the most part)

--- INSTALLER.py
+++ INSTALLER.py
@@ -1,5 +1,6 @@
 import os
 import json
+import getpass
 import platform
 import requests
 
@@ -21,9 +22,32 @@ if not releases.ok:
     print("Couldn't fetch releases. Exiting...")
     exit()
 
+paths = {
+    "Windows": [
+        f"C:/Users/{getpass.getuser()}/AppData/Local/BetterVencordPatch/vencordinstaller.exe",
+        f"C:/Users/{getpass.getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/autovencordpatch.exe",
+    ],
+    "Darwin": [
+        f"/Applications/VencordInstaller.app",
+        f"/Applications/VencordInstaller.app/Contents/Resources/autovencordpatch",
+    ],
+}
+
+clear()
+print("[Downloading required files...]")
 rel = json.loads(releases.text)
 for asset in rel[0]["assets"]:
-    if f"VencordInstaller-{"no_" if not openasar else ""}openasar{suffix}" in asset["browser_download_url"]:
-        open(f"VencordInstaller{suffix}", "wb").write(requests.get(asset["browser_download_url"]).content)
-        input(f"Successfully downloaded BetterVencordPatch {rel[0]["name"]}! ")
-        break
+    if f"VencordInstaller-{"no_" if not openasar else ""}openasar.exe" in asset["browser_download_url"] and platform.system() == "Windows":
+        open(paths[platform.system()][0], "wb").write(requests.get(asset["browser_download_url"]).content)
+        print(f"Successfully downloaded BetterVencordPatch")
+    elif f"VencordInstaller-{"no_" if not openasar else ""}openasar.app" in asset["browser_download_url"] and platform.system() == "Darwin":
+        open("VencordInstaller.app.zip", "wb").write(requests.get(asset["browser_download_url"]).content)
+        # TODO: add .zip unzipping code here for .app files
+        # TODO: move extracted app to paths[platform.system()][0]
+        print(f"Successfully downloaded BetterVencordPatch")
+    elif f"autovencordpatch{".exe" if platform.system() == "Windows" else ""}" in asset["browser_download_url"] and autopatch:
+        open(paths[platform.system()][1], "wb").write(requests.get(asset["browser_download_url"]).content)
+        print(f"Successfully installed autopatch component")
+    elif "org.aaron.autovencordpatch.plist" in asset["browser_download_url"] and platform.system() == "Darwin":
+        open(f"~/Library/LaunchAgents/org.aaron.autovencordpatch.plist", "wb").write(requests.get(asset["browser_download_url"]).content)
+        print(f"Successfully installed autopatch plist (macOS)")
