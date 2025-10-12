9609387b191645ac58f20cf2e04cc08882809bbf
Aaron Wijesinghe <aaronw1975@icloud.com>
2025-10-12 16:48:01 -0400

    Update installer and remove binaries that shouldn't exist

--- INSTALLER.py
+++ INSTALLER.py
@@ -1,10 +1,11 @@
 import os
 import json
+import shutil
+import zipfile
 import getpass
 import platform
 import requests
 
-suffix = ".app" if platform.system() == "Darwin" else ".exe"
 os.chdir(os.path.dirname(__file__))
 def clear():
     os.system("cls" if platform.system() == "Windows" else "clear;clear")
@@ -13,10 +14,9 @@ clear()
 print("[BetterVencordPatch Installer]")
 print("This installer will download the latest files from GitHub.")
 print("")
-autopatch = input("Automatically patch Discord with Vencord through updates? ").lower().strip() == "y"
+autopatch = input("Automatically patch Discord with Vencord through updates (y/n)? ").lower().strip() == "y"
 openasar = input("Patch OpenAsar (y/n)? ").lower().strip() == "y"
 
-os.chdir(os.path.dirname(__file__))
 releases = requests.get("https://api.github.com/repos/introvertednoob/bettervencordpatch/releases")
 if not releases.ok:
     print("\nCouldn't fetch releases. Exiting...")
@@ -33,26 +33,42 @@ paths = {
     ],
 }
 
-if platform.system() == "Windows" and not os.path.exists(paths["Windows"][0]+"/.."):
-    os.mkdir(paths["Windows"][0]+"/..")
+if platform.system() == "Windows":
+    os.system("taskkill /f /im autovencordpatch.exe >NUL 2>&1")
+    os.makedirs(f"C:/Users/{getpass.getuser()}/AppData/Local/BetterVencordPatch/", exist_ok=True)
 
 clear()
 print("[Downloading and moving required files...]")
 rel = json.loads(releases.text)
 for asset in rel[0]["assets"]:
-    if f"VencordInstaller-{"no_" if not openasar else ""}openasar.exe" in asset["browser_download_url"] and platform.system() == "Windows":
-        open(paths[platform.system()][0], "wb").write(requests.get(asset["browser_download_url"]).content)
-        print(f"Successfully downloaded BetterVencordPatch")
-    elif f"VencordInstaller-{"no_" if not openasar else ""}openasar.app" in asset["browser_download_url"] and platform.system() == "Darwin":
-        open("VencordInstaller.app.zip", "wb").write(requests.get(asset["browser_download_url"]).content)
-        # TODO: add .zip unzipping code here for .app files
-        # TODO: move extracted .app to paths[platform.system()][0]
-        print(f"Successfully downloaded BetterVencordPatch")
-    elif f"autovencordpatch{".exe" if platform.system() == "Windows" else ""}" in asset["browser_download_url"] and autopatch:
-        open(paths[platform.system()][1], "wb").write(requests.get(asset["browser_download_url"]).content)
-        print(f"Successfully installed autopatch component")
-    elif "org.aaron.autovencordpatch.plist" in asset["browser_download_url"] and platform.system() == "Darwin":
-        open(f"~/Library/LaunchAgents/org.aaron.autovencordpatch.plist", "wb").write(requests.get(asset["browser_download_url"]).content)
-        print(f"Successfully installed autopatch plist (macOS)")
+    if platform.system() == "Darwin":
+        if f"VencordInstaller-{"no_" if not openasar else ""}openasar.app.zip" == asset["name"]:
+            open("VencordInstaller.app.zip", "wb").write(requests.get(asset["browser_download_url"]).content)
+            if os.path.exists("/Applications/VencordInstaller.app"):
+                shutil.rmtree("/Applications/VencordInstaller.app")
+            with zipfile.ZipFile("VencordInstaller.app.zip", 'r') as zip_ref:
+                zip_ref.extractall("/Applications/")
+            shutil.move(f"/Applications/VencordInstaller-{"no_" if not openasar else ""}openasar.app", "/Applications/VencordInstaller.app")
+            os.system("chmod +x /Applications/VencordInstaller.app/Contents/MacOS/vencordinstaller")
+            print(f"Successfully downloaded BetterVencordPatch")
+    elif platform.system() == "Windows":
+        if f"VencordInstaller-{"no_" if not openasar else ""}openasar.exe" == asset["name"]:
+            open(f"C:/Users/{getpass.getuser()}/AppData/Local/BetterVencordPatch/vencordinstaller.exe", "wb").write(requests.get(asset["browser_download_url"]).content)
+            print(f"Successfully downloaded BetterVencordPatch")
+        elif f"autovencordpatch.exe" == asset["name"] and autopatch:
+            open(f"C:/Users/{getpass.getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/autovencordpatch.exe", "wb").write(requests.get(asset["browser_download_url"]).content)
+            print(f"Successfully installed autopatch component")
+
+if platform.system() == "Darwin":
+    for asset in rel[0]["assets"]:
+        if asset["name"] == "org.aaron.autovencordpatch.plist":
+            open(f"/Users/{getpass.getuser()}/Library/LaunchAgents/org.aaron.autovencordpatch.plist", "wb").write(requests.get(asset["browser_download_url"]).content)
+            print(f"Successfully installed autopatch launchd plist (macOS)")
+        elif asset["name"] == "autovencordpatch" and autopatch:
+            open(f"/Applications/VencordInstaller.app/Contents/Resources/autovencordpatch", "wb").write(requests.get(asset["browser_download_url"]).content)
+            os.system("chmod +x /Applications/VencordInstaller.app/Contents/Resources/autovencordpatch")
+            print(f"Successfully installed autopatch component")
+    os.system("open /Applications/VencordInstaller.app")
+
 input("\nSuccessfully installed BetterVencordPatch! ")
 exit()
