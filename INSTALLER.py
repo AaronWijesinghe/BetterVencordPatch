ad56a8375865e8aeebd6c1f663d05d0099a5ab71
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-08-12 18:41:46 -0400

    feat(INSTALLER): use requests.Session() for faster download times

--- INSTALLER.py
+++ INSTALLER.py
@@ -16,6 +16,16 @@ os.chdir(os.path.dirname(__file__))
 def clear():
     print("\033[2J\033[3J\033[H", end='')
 
+def download_file(session, url):
+    try:
+        file_request = session.get(url)
+        if not file_request.ok:
+            raise requests.exceptions.ConnectionError
+        return file_request.content
+    except requests.exceptions.ConnectionError:
+        input(f"Couldn't fetch file from URL: {url}")
+        exit()
+
 clear()
 print(f"{bold}{gold}[BetterVencordPatch Installer]{end}")
 
@@ -59,11 +69,13 @@ if platform.system() == "Windows":
     os.makedirs(f"C:/Users/{getpass.getuser()}/AppData/Local/BetterVencordPatch/", exist_ok=True)
 
 clear()
+session = requests.Session()
 print(f"{bold}{gold}[Downloading and moving required files...]{end}")
 for asset in releases[0]["assets"]:
     if platform.system() == "Darwin":
         if f"VencordInstaller-{"no_" if not openasar else ""}openasar.app.zip" == asset["name"]:
-            open("VencordInstaller.app.zip", "wb").write(requests.get(asset["browser_download_url"]).content)
+            vi_app_darwin = download_file(session, asset["browser_download_url"])
+            open("VencordInstaller.app.zip", "wb").write(vi_app_darwin)
             if os.path.exists("/Applications/VencordInstaller.app"):
                 shutil.rmtree("/Applications/VencordInstaller.app")
             with zipfile.ZipFile("VencordInstaller.app.zip", 'r') as zip_ref:
@@ -74,26 +86,31 @@ for asset in releases[0]["assets"]:
             print(f"Successfully downloaded BetterVencordPatch")
     elif platform.system() == "Windows":
         if f"VencordInstaller-{"no_" if not openasar else ""}openasar.exe" == asset["name"]:
-            open(f"C:/Users/{getpass.getuser()}/AppData/Local/BetterVencordPatch/vencordinstaller.exe", "wb").write(requests.get(asset["browser_download_url"]).content)
+            vi_app_win = download_file(session, asset["browser_download_url"])
+            open(f"C:/Users/{getpass.getuser()}/AppData/Local/BetterVencordPatch/vencordinstaller.exe", "wb").write(vi_app_win)
             print(f"Successfully downloaded BetterVencordPatch")
         elif f"autovencordpatch.exe" == asset["name"] and autopatch:
-            open(f"C:/Users/{getpass.getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/autovencordpatch.exe", "wb").write(requests.get(asset["browser_download_url"]).content)
+            autopatch_win = download_file(session, asset["browser_download_url"])
+            open(f"C:/Users/{getpass.getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/autovencordpatch.exe", "wb").write(autopatch_win)
             print(f"Successfully installed autopatch component")
 
 if platform.system() == "Darwin":
     for asset in releases[0]["assets"]:
         if asset["name"] == "org.aaron.autovencordpatch.plist":
-            open(f"/Users/{getpass.getuser()}/Library/LaunchAgents/org.aaron.autovencordpatch.plist", "wb").write(requests.get(asset["browser_download_url"]).content)
+            autopatch_plist = download_file(session, asset["browser_download_url"])
+            open(f"/Users/{getpass.getuser()}/Library/LaunchAgents/org.aaron.autovencordpatch.plist", "wb").write(autopatch_plist)
             os.system("chmod 644 ~/Library/LaunchAgents/org.aaron.autovencordpatch.plist")
             os.system("launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/org.aaron.autovencordpatch.plist 2>&1")
             os.system("launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/org.aaron.autovencordpatch.plist 2>&1")
             print(f"Successfully installed autopatch launchd plist (macOS)")
         elif asset["name"] == "autovencordpatch" and autopatch:
-            open(f"/Applications/VencordInstaller.app/Contents/Resources/autovencordpatch", "wb").write(requests.get(asset["browser_download_url"]).content)
+            autopatch_darwin = download_file(session, asset["browser_download_url"])
+            open(f"/Applications/VencordInstaller.app/Contents/Resources/autovencordpatch", "wb").write(autopatch_darwin)
             os.system("chmod +x /Applications/VencordInstaller.app/Contents/Resources/autovencordpatch")
             print(f"Successfully installed autopatch component")
     os.system("open /Applications/VencordInstaller.app")
 
+session.close()
 print("\nSuccessfully installed BetterVencordPatch!")
 if platform.system() == "Windows" and autopatch == True:
     input("Make sure to restart your computer so the auto-patcher can run.")
